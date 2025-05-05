dc3e7f6525a00af58116dd7748154225ad772928
Yashigami <kuzmin_var@bk.ru>
2025-05-05 20:08:50 +0300

    Добавил Swagger-документацию и покдлючил docs

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	_ "effective-mobail/docs"
 	"effective-mobail/internal/config"
 	"effective-mobail/internal/handler"
 	"effective-mobail/internal/storage"
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"log"
 	"net/http"
 )
+
+// @title People API
+// @version 1.0
+// @description API-сервис для обогащения управления данными пользователя.
+// @host localhost:8080
+// @BasePath /
 
 func main() {
 	cfg := config.LoadConfig() // Загружаю конфиг из .env
@@ -26,6 +34,8 @@ func main() {
 	r.HandleFunc("/people", h.GetPeople).Methods("GET")
 	r.HandleFunc("/people/{id}", h.UpdatePerson).Methods("PUT")
 	r.HandleFunc("/people/{id}", h.DeletePerson).Methods("DELETE")
+
+	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
 	log.Println("Сервер запущен на порту:", cfg.Port)
 	err := http.ListenAndServe(":"+cfg.Port, r)
